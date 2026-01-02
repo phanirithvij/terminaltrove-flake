72e28e18f8bd7fcf9aec58bfc8d1c4f40d39332f
phanirithvij <phanirithvij2000@gmail.com>
2026-01-02 13:45:16 +0530

    joecalsend: add meta, checks
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/by-name/joecalsend/package.nix
+++ pkgs/by-name/joecalsend/package.nix
@@ -1,22 +1,60 @@
 {
-  pkgs,
+  lib,
   rustPlatform,
   fetchFromGitea,
+
+  openssl,
+  pkg-config,
+
+  versionCheckHook,
+  nix-update-script,
 }:
-rustPlatform.buildRustPackage {
-  name = "joecalsend";
+rustPlatform.buildRustPackage rec {
+  pname = "joecalsend";
+  # see https://git.kittencollective.com/nebkor/joecalsend/src/branch/main/VERSIONING.md
+  version = "1.6.18033988";
 
   src = fetchFromGitea {
     domain = "git.kittencollective.com";
     owner = "nebkor";
     repo = "joecalsend";
-    tag = "1.61803398";
-    hash = "sha256-7Gl+G4BN3CgF0c/AEhI1OvRhveqGeFNmGRI3XRf6rAo=";
+    tag =
+      let
+        # Remove middle dot from version string (1.6.18033988 -> 1.618033988)
+        # because GitHub release tags use the condensed format
+        parts = builtins.splitVersion version;
+        ver = "${builtins.elemAt parts 0}.${builtins.elemAt parts 1}${builtins.elemAt parts 2}";
+      in
+      ver;
+    hash = "sha256-nzsvVC1e8ENh0bpQwiogGew823NNmSNXN+VZZHfVFIY=";
   };
 
-  nativeBuildInputs = with pkgs; [ pkg-config ];
+  cargoHash = "sha256-5V/a6rj08Ucu6S+SBukYQktWLVnnbXeoGan1oYTozHc=";
 
-  buildInputs = with pkgs; [ openssl ];
+  nativeBuildInputs = [ pkg-config ];
 
-  cargoHash = "sha256-prT1wO3ctnTfMHfICFcihB739lN/QXPH3AamIR6dM9A=";
+  buildInputs = [ openssl ];
+
+  postInstall = ''
+    ln -s $out/bin/jocalsend $out/bin/joecalsend
+  '';
+
+  doInstallCheck = true;
+  versionCheckProgramArg = "--version";
+  nativeInstallCheckInputs = [ versionCheckHook ];
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    changelog = "https://git.kittencollective.com/nebkor/joecalsend/releases/tag/${version}";
+    description = "Rust terminal client for Localsend";
+    homepage = "https://git.kittencollective.com/nebkor/joecalsend";
+    # https://git.kittencollective.com/nebkor/joecalsend/src/branch/main/LICENSE.md
+    license = lib.licenses.unfree; # anti-llm and another non-osi approved license
+    mainProgram = "jocalsend";
+    maintainers = with lib.maintainers; [
+      ipsavitsky
+      phanirithvij
+    ];
+  };
 }
