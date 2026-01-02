e24d2539aa10d2a1a0307eaef7c5a8a3f3376d00
phanirithvij <phanirithvij2000@gmail.com>
2026-01-02 13:45:24 +0530

    loggo: add meta, checks
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/by-name/loggo/package.nix
+++ pkgs/by-name/loggo/package.nix
@@ -1,14 +1,60 @@
-{ buildGoModule, fetchFromGitHub }:
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
 
-buildGoModule rec {
-  name = "loggo";
-  version = "v0.3.21";
+  versionCheckHook,
+  writableTmpDirAsHomeHook,
+
+  nix-update-script,
+}:
+
+buildGoModule (finalAttrs: {
+  pname = "loggo";
+  version = "0.3.21";
+
   src = fetchFromGitHub {
     owner = "aurc";
     repo = "loggo";
-    tag = version;
+    tag = "v${finalAttrs.version}";
     hash = "sha256-j+vrfEmgMRfR0iE/67z+s2icRR57igEP3UaVduVaYK0=";
   };
+
   vendorHash = "sha256-lUGDWsU/5g6TcC4WgEh/RCshgkD/XtXzC8O0VoNa6fs=";
-  doCheck = false;
-}
+
+  ldflags = [
+    "-s"
+    "-X main.version=${finalAttrs.version}"
+  ];
+
+  # can do subPackages=["."] but checkphase is not finding the tests
+  postInstall = ''
+    pushd $out/bin
+    find . ! -name loggo -delete
+    popd
+  '';
+
+  nativeCheckInputs = [ writableTmpDirAsHomeHook ];
+
+  doInstallCheck = true;
+  versionCheckProgramArg = "version";
+  nativeInstallCheckInputs = [
+    versionCheckHook
+    writableTmpDirAsHomeHook
+  ];
+  versionCheckKeepEnvironment = [ "HOME" ];
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    changelog = "https://github.com/aurc/loggo/releases/tag/${finalAttrs.src.tag}";
+    description = "Powerful terminal app for structured log streaming";
+    homepage = "https://github.com/aurc/loggo";
+    license = lib.licenses.mit;
+    mainProgram = "loggo";
+    maintainers = with lib.maintainers; [
+      ipsavitsky
+      phanirithvij
+    ];
+  };
+})
