8551b745f2a24ef9454476b75c1d9cb003cf6a6e
phanirithvij <phanirithvij2000@gmail.com>
2026-01-02 13:44:48 +0530

    awesesh: add meta, checks
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- pkgs/by-name/awsesh/package.nix
+++ pkgs/by-name/awsesh/package.nix
@@ -1,13 +1,42 @@
-{ buildGoModule, fetchFromGitHub }:
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+  versionCheckHook,
+  nix-update-script,
+}:
 
-buildGoModule rec {
-  name = "awsesh";
-  version = "v.0.1.4";
+buildGoModule (finalAttrs: {
+  pname = "awsesh";
+  version = "0.1.11";
   src = fetchFromGitHub {
     owner = "elva-labs";
     repo = "awsesh";
-    tag = version;
-    hash = "sha256-IJd6l+04ie8jiBgmpbWr/txKJDAzetXQqyb5naZSGBg=";
+    tag = "v.${finalAttrs.version}";
+    hash = "sha256-14f6Wa+UZaXNq45D+bLeb4M5gui1BBdSgycI+bYc4MI=";
   };
   vendorHash = "sha256-hGwGvE9Y0awezAijHMt5heBERcV92olugCaMzzvDvKc=";
-}
+
+  ldflags = [
+    "-s"
+    "-X main.Version=${finalAttrs.version}"
+  ];
+
+  doInstallCheck = true;
+  versionCheckProgramArg = "--version";
+  nativeInstallCheckInputs = [ versionCheckHook ];
+
+  passthru.updateScript = nix-update-script { };
+
+  meta = {
+    changelog = "https://github.com/elva-labs/awsesh/releases/tag/${finalAttrs.src.tag}";
+    description = "TUI for AWS SSO session management";
+    homepage = "https://github.com/elva-labs/awsesh";
+    license = lib.licenses.mit;
+    mainProgram = "awsesh";
+    maintainers = with lib.maintianers; [
+      ipsavitsky
+      phanirithvij
+    ];
+  };
+})
